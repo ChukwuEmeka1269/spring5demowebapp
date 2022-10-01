e1bd45e3d6829a97e258bf03bca227952b75670b
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-10-01 19:14:21 +0100

    Domain entity -  Added a domain entity "author".

---
+++ src/main/java/com/rexcoweb/spring5webapp/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.rexcoweb.spring5webapp.repository;
+
+import com.rexcoweb.spring5webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+}
